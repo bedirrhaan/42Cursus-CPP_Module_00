6ec7a66d3f4007623c59c316cdf1bedff2787b40
bedirrhaan <bedirrhaan@gmail.com>
2024-03-09 01:49:41 +0300

    42

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -15,14 +15,19 @@ bool	PhoneBook::PhoneBookAdd()
 	static int index = 0;
 	std::string	line;
 
+	std::cout << "Name : ";
 	getline(std::cin, line);
 	_contact[index % 8].setName(line);
+	std::cout << "Lastname : ";
 	getline(std::cin, line);
 	_contact[index % 8].setLastname(line);
+	std::cout << "Username : ";
 	getline(std::cin, line);
 	_contact[index % 8].setUsername(line);
+	std::cout << "Telephone Number : ";
 	getline(std::cin, line);
 	_contact[index % 8].setTelephoneNumber(line);
+	std::cout << "Dark Secret : ";
 	getline(std::cin, line);
 	_contact[index % 8].setDarkSecret(line);
 	setContact(_contact[index], index);
@@ -32,6 +37,11 @@ bool	PhoneBook::PhoneBookAdd()
 
 bool	PhoneBook::PhoneBookSearch(void)
 {
+	std::cout << "Name = " << _contact[0].getName() << std::endl;
+	std::cout << "Lastname = " << _contact[0].getLastname() << std::endl;
+	std::cout << "Username = " << _contact[0].getUsername() << std::endl;
+	std::cout << "Telephone Number = " << _contact[0].getTelephoneNumber() << std::endl;
+	std::cout << "Dark Secret = " << _contact[0].getDarkSecret() << std::endl;
 
 	return (true);
 }
